0b16dd8ae98fda42b4330e374ce5802169aace2a
Levin <levindw05@gmail.com>
2025-05-19 22:39:52 +0800

    hal akun, navbar berfungsi

--- ALP-Canteen-struktur-frontend/cipeats/lib/main.dart
+++ ALP-Canteen-struktur-frontend/cipeats/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cipeats/view/pesanan.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'view/home_page.dart';
@@ -7,6 +8,8 @@ import 'view/menu_detail_page.dart';
 import 'view/loginPage.dart';
 import 'view/splash_screen.dart';
 import 'view/register_page.dart';
+import 'view/akun.dart';
+import 'view/pesanan.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -25,13 +28,15 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/splash',
       routes: {
-        '/': (context) => const HomePage(),
+        '/home': (context) => const HomePage(),
          '/splash': (context) => const SplashScreen(),
          '/login': (context) => const LoginPage(),   
          '/register': (context) => const RegisterPage(),         
         '/vendor1': (context) => const Vendor1Page(), 
         '/vendor2': (context) => const Vendor2Page(), 
-        '/detail': (context) => const MenuDetailPage(), 
+        '/detail': (context) => const MenuDetailPage(),
+        '/akun': (context) => const AkunPage(), 
+        '/pesanan': (context) => const PesananPage(), 
       },
     );
   }
